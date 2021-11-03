1c91584b39ea862838ad615ad20e7d9031e6b3ea
seongmin <psm7335@naver.com>
2021-11-04 00:04:56 +0900

    로그인

--- src/main/java/glowa/glowabackendspring/apiController/UserApiController.java
+++ src/main/java/glowa/glowabackendspring/apiController/UserApiController.java
@@ -2,10 +2,12 @@ package glowa.glowabackendspring.apiController;
 
 import com.sun.istack.NotNull;
 import glowa.glowabackendspring.exception.BindingException;
+import glowa.glowabackendspring.exception.ConflictException;
 import glowa.glowabackendspring.exception.LoginException;
 import glowa.glowabackendspring.exhandler.ErrorResult;
 import glowa.glowabackendspring.service.UserService;
 import glowa.glowabackendspring.entity.User;
+import glowa.glowabackendspring.session.SessionConst;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,8 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
@@ -28,11 +32,11 @@ public class UserApiController {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
 
-    @ResponseStatus(HttpStatus.CONFLICT)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
     public ErrorResult loginExHandler(LoginException e) {
         log.error("[exceptionHandler] ex", e);
-        return new ErrorResult("409", e.getMessage());
+        return new ErrorResult("400", e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -42,11 +46,16 @@ public class UserApiController {
         return new ErrorResult("400", e.getMessage());
     }
 
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler
+    public ErrorResult bindingExHandler(ConflictException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("409", e.getMessage());
+    }
+
     @PostMapping("/join") //회원가입
     public CreateUserResponse saveUser(@RequestBody @Validated CreateUserRequest request, BindingResult bindingResult) {
-        if(bindingResult.hasErrors()) {
-            throw new BindingException(bindingResult.getFieldErrors().get(0).getDefaultMessage());
-        }
+        bindingResultException(bindingResult);
 
         String encodedPassword = passwordEncoder.encode(request.password);
         User user = new User(request.userId, request.nickname, encodedPassword);
@@ -54,6 +63,41 @@ public class UserApiController {
         Long id = userService.join(user);
 
         return new CreateUserResponse(id, request.userId, request.nickname);
+    }
+
+    @PostMapping("/auth/login")
+    public LoginUserResponse login(@RequestBody @Validated LoginUserRequest request, BindingResult bindingResult, HttpServletRequest req) {
+        bindingResultException(bindingResult);
+        User user = userService.login(request.userId, request.password);
+
+        HttpSession session = req.getSession();
+        session.setAttribute(SessionConst.LOGIN_USER, user);
+
+        return new LoginUserResponse(user.getId(), user.getNickname());
+    }
+
+    @GetMapping("/auth/login")
+    public LoginUserResponse login(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) User user){
+        if(user == null) {
+            throw new LoginException("로그인 되어있지 않음");
+        }
+
+        return new LoginUserResponse(user.getId(), user.getNickname());
+    }
+
+    @GetMapping("/auth/logout")
+    public String logout(HttpServletRequest req) {
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "200";
+    }
+
+    private void bindingResultException(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new BindingException(bindingResult.getFieldErrors().get(0).getDefaultMessage());
+        }
     }
 
     @Data
@@ -71,6 +115,21 @@ public class UserApiController {
     static class CreateUserResponse {
         private Long id;
         private String userId;
+        private String nickname;
+    }
+
+    @Data
+    static class LoginUserRequest {
+        @NotEmpty(message = "아이디 값은 비어있을 수 없습니다.")
+        private String userId;
+        @NotEmpty(message = "패스워드 값은 비어있을 수 없습니다.")
+        private String password;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class LoginUserResponse {
+        private Long id;
         private String nickname;
     }
 }
--- src/main/java/glowa/glowabackendspring/service/UserService.java
+++ src/main/java/glowa/glowabackendspring/service/UserService.java
@@ -1,21 +1,26 @@
 package glowa.glowabackendspring.service;
 
 import glowa.glowabackendspring.entity.User;
+import glowa.glowabackendspring.exception.ConflictException;
 import glowa.glowabackendspring.exception.LoginException;
 import glowa.glowabackendspring.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public Long join(User user) { //회원가입
@@ -24,22 +29,25 @@ public class UserService {
         return user.getId();
     }
 
-    public List<User> findByUserId(String userId) {
-        return userRepository.findByUserId(userId);
-    }
-
-    public List<User> findByNickname(String nickname) {
-        return userRepository.findByNickname(nickname);
+    public User login(String loginId, String password) {
+        User user = userRepository.findByUserId(loginId).orElse(null);
+        if(user == null) {
+            throw new LoginException("잘못된 아이디입니다.");
+        }
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new LoginException("잘못된 비밀번호입니다.");
+        }
+        return user;
     }
 
     private void validateDuplicateUser(User user) {
-        List<User> findByUserId = userRepository.findByUserId(user.getUserId());
-        if(!findByUserId.isEmpty()) {
-            throw new LoginException("이미 사용중인 아이디입니다.");
+        Optional<User> findByUserId = userRepository.findByUserId(user.getUserId());
+        if(findByUserId.isPresent()) {
+            throw new ConflictException("이미 사용중인 아이디입니다.");
         }
-        List<User> findByNickname = userRepository.findByNickname(user.getNickname());
-        if(!findByNickname.isEmpty()) {
-            throw new LoginException("이미 사용중인 닉네임입니다.");
+        Optional<User> findByNickname = userRepository.findByNickname(user.getNickname());
+        if(findByNickname.isPresent()) {
+            throw new ConflictException("이미 사용중인 닉네임입니다.");
         }
     }
 
