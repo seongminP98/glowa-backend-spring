be3a8bac7ad37aafc890e17ab3ee04b3d1633d59
seongmin <psm7335@naver.com>
2021-11-08 16:30:13 +0900

    create InvSchedule domain

--- src/main/java/glowa/glowabackendspring/domain/InvSchedule.java
+++ src/main/java/glowa/glowabackendspring/domain/InvSchedule.java
@@ -1,4 +1,30 @@
 package glowa.glowabackendspring.domain;
 
-public class InvSchedule {
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InvSchedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "my_id")
+    private User me;
+
+    @ManyToOne
+    @JoinColumn(name = "friend_id")
+    private User friend;
+
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+    
 }
