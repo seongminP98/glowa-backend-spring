418e4a1fd71b5b26d0a5adef6c70a3a5c1d69874
seongmin <psm7335@naver.com>
2021-11-19 16:53:00 +0900

    create friend Repository

--- src/main/java/glowa/glowabackendspring/repository/reqFriend/FriendRepository.java
+++
@@ -1,7 +0,0 @@
-package glowa.glowabackendspring.repository.reqFriend;
-
-import glowa.glowabackendspring.domain.Friend;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FriendRepository extends JpaRepository<Friend, Long> {
-}
