be81bc47d27efd0ee92419129b0182e4d58ff5a3
seongmin <psm7335@naver.com>
2021-11-22 18:05:51 +0900

    add schedule repository test findOneById

--- src/test/java/glowa/glowabackendspring/repository/schedule/ScheduleRepositoryTest.java
+++ src/test/java/glowa/glowabackendspring/repository/schedule/ScheduleRepositoryTest.java
@@ -3,6 +3,7 @@ package glowa.glowabackendspring.repository.schedule;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import glowa.glowabackendspring.domain.Schedule;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,8 +27,8 @@ public class ScheduleRepositoryTest {
     @Autowired
     ScheduleRepository scheduleRepository;
 
-    @Test
-    public void findByMasterAndName() {
+    @BeforeEach
+    public void before() {
         queryFactory = new JPAQueryFactory(em);
 
         Schedule schedule1 = new Schedule(1L, "점심약속", LocalDateTime.now(), "집 앞");
@@ -37,13 +38,22 @@ public class ScheduleRepositoryTest {
         em.persist(schedule1);
         em.persist(schedule2);
         em.persist(schedule3);
+    }
 
-        em.flush();
-        em.clear();
-
+    @Test
+    public void findByMasterAndName() {
         Schedule result = scheduleRepository.findOneByMasterAndName(1L, "점심약속");
         System.out.println("result = " + result);
         assertThat(result.getName()).isEqualTo("점심약속");
 
     }
+
+    @Test
+    public void findOneById() {
+        Schedule result = scheduleRepository.findOneById(2L);
+        System.out.println("result = " + result);
+        assertThat(result.getMaster()).isEqualTo(1);
+        assertThat(result.getName()).isEqualTo("저녁약속");
+    }
+
 }
