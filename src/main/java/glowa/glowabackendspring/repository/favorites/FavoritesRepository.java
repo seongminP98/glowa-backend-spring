c53a9b581f532e5c017ce3f9adc748f887a5e208
seongmin <psm7335@naver.com>
2021-11-23 16:46:26 +0900

    add favorited repository findOneByUserAndAddress

---
+++ src/main/java/glowa/glowabackendspring/repository/favorites/FavoritesRepository.java
@@ -0,0 +1,11 @@
+package glowa.glowabackendspring.repository.favorites;
+
+import glowa.glowabackendspring.domain.Favorites;
+import glowa.glowabackendspring.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FavoritesRepository extends JpaRepository<Favorites, Long> {
+    Optional<Favorites> findOneByUserAndAddress(User user, String address);
+}
