05ea13aefe0ed2e02db038d11c612ad99dfe4d23
seongmin <psm7335@naver.com>
2021-11-24 18:13:59 +0900

    create userException

---
+++ src/main/java/glowa/glowabackendspring/exception/UserException.java
@@ -0,0 +1,10 @@
+package glowa.glowabackendspring.exception;
+
+public class UserException extends RuntimeException {
+    public UserException() {
+    }
+
+    public UserException(String message) {
+        super(message);
+    }
+}
