5532289cf96fd4967df900299666de7738f90c02
seongmin <psm7335@naver.com>
2021-11-25 18:08:58 +0900

    fix reqFriendRepository

--- src/main/java/glowa/glowabackendspring/repository/reqFriend/ReqFriendRepository.java
+++ src/main/java/glowa/glowabackendspring/repository/reqFriend/ReqFriendRepository.java
@@ -1,6 +1,7 @@
 package glowa.glowabackendspring.repository.reqFriend;
 
 import glowa.glowabackendspring.domain.ReqFriends;
+import glowa.glowabackendspring.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +9,5 @@ import java.util.Optional;
 
 public interface ReqFriendRepository extends JpaRepository<ReqFriends, Long>, ReqFriendRepositoryCustom {
     List<ReqFriends> findAllById(Long id);
-    Optional<ReqFriends> findOneByMeAndReqFriend(Long myId, Long friendId);
+    Optional<ReqFriends> findOneByMeAndReqFriend(User my, User friend);
 }
