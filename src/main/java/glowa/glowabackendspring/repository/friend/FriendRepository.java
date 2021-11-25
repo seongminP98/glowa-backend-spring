6615ee167dbe384bd946bda3101b0add2f4a0b77
seongmin <psm7335@naver.com>
2021-11-25 18:08:42 +0900

    fix friendRepository

--- src/main/java/glowa/glowabackendspring/repository/friend/FriendRepository.java
+++ src/main/java/glowa/glowabackendspring/repository/friend/FriendRepository.java
@@ -5,8 +5,10 @@ import glowa.glowabackendspring.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FriendRepository extends JpaRepository<Friend, Long>, FriendRepositoryCustom {
     List<Friend> findAllByMe(User user);
+    Optional<Friend> findOneByMeAndFriend(User me, User friend);
     long deleteByMeAndFriend(User me, User friend);
 }
