118503e6d360853f101714db8b8784dc69bd3d55
seongmin <psm7335@naver.com>
2021-11-25 17:41:47 +0900

    modify reqFriendRepository alreadyReq

--- src/main/java/glowa/glowabackendspring/repository/reqFriend/ReqFriendRepository.java
+++ src/main/java/glowa/glowabackendspring/repository/reqFriend/ReqFriendRepository.java
@@ -4,7 +4,9 @@ import glowa.glowabackendspring.domain.ReqFriends;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReqFriendRepository extends JpaRepository<ReqFriends, Long>, ReqFriendRepositoryCustom {
     List<ReqFriends> findAllById(Long id);
+    Optional<ReqFriends> findOneByMeAndReqFriend(Long myId, Long friendId);
 }
