dcfe2073ee038a6dc75dbf84c0b567a7714fce33
seongmin <psm7335@naver.com>
2021-11-30 17:35:51 +0900

    rename AddFriendException > FriendException

--- src/main/java/glowa/glowabackendspring/service/ReqFriendService.java
+++ src/main/java/glowa/glowabackendspring/service/ReqFriendService.java
@@ -4,17 +4,15 @@ import glowa.glowabackendspring.domain.Friend;
 import glowa.glowabackendspring.domain.ReqFriends;
 import glowa.glowabackendspring.domain.User;
 import glowa.glowabackendspring.dto.user.UserInfoDto;
-import glowa.glowabackendspring.exception.AddFriendException;
+import glowa.glowabackendspring.exception.FriendException;
 import glowa.glowabackendspring.repository.friend.FriendRepository;
 import glowa.glowabackendspring.repository.reqFriend.ReqFriendRepository;
 import glowa.glowabackendspring.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,22 +29,22 @@ public class ReqFriendService {
     public ReqFriends addFriend(User me, Long friendId) {
         Optional<User> friend = userRepository.findById(friendId);
         if(friend.isEmpty()) {
-            throw new AddFriendException("잘못된 친구요청입니다.");
+            throw new FriendException("잘못된 친구요청입니다.");
         }
 
         Optional<ReqFriends> alreadyReq = reqFriendRepository.findOneByMeAndReqFriend(me, friend.get());
         if(alreadyReq.isPresent()) {
-            throw new AddFriendException("이미 친구요청을 보냈습니다.");
+            throw new FriendException("이미 친구요청을 보냈습니다.");
         }
 
         Optional<ReqFriends> receiveReq = reqFriendRepository.findOneByMeAndReqFriend(friend.get(), me);
         if(receiveReq.isPresent()) {
-            throw new AddFriendException("상대한테 친구요청을 받은 상태입니다.");
+            throw new FriendException("상대한테 친구요청을 받은 상태입니다.");
         }
 
         Optional<Friend> alreadyFriend = friendRepository.findOneByMeAndFriend(me, friend.get());
         if(alreadyFriend.isPresent()) {
-            throw new AddFriendException("이미 친구입니다.");
+            throw new FriendException("이미 친구입니다.");
         }
 
         return reqFriendRepository.save(new ReqFriends(me, friend.get()));
