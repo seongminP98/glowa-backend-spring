32221643a312a4ef563deea00250fc77a96b6509
seongmin <psm7335@naver.com>
2021-11-30 22:34:25 +0900

    add FriendRepository friendList

--- src/main/java/glowa/glowabackendspring/repository/friend/FriendRepositoryCustom.java
+++ src/main/java/glowa/glowabackendspring/repository/friend/FriendRepositoryCustom.java
@@ -1,7 +1,11 @@
 package glowa.glowabackendspring.repository.friend;
 
 import glowa.glowabackendspring.domain.Friend;
+import glowa.glowabackendspring.dto.user.UserInfoDto;
+
+import java.util.List;
 
 public interface FriendRepositoryCustom {
     Friend friend(Long myId, Long friendId);
+    List<UserInfoDto> friendList(Long myId);
 }
