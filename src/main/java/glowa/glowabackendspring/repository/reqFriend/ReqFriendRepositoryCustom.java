af6b4d2824ad3880ecc0b9b711ec20f8d2a95b01
seongmin <psm7335@naver.com>
2021-11-30 17:33:37 +0900

    modify reqFriendRepository reqList

--- src/main/java/glowa/glowabackendspring/repository/reqFriend/ReqFriendRepositoryCustom.java
+++ src/main/java/glowa/glowabackendspring/repository/reqFriend/ReqFriendRepositoryCustom.java
@@ -1,12 +1,13 @@
 package glowa.glowabackendspring.repository.reqFriend;
 
 import glowa.glowabackendspring.domain.ReqFriends;
+import glowa.glowabackendspring.dto.user.UserInfoDto;
 
 import java.util.List;
 
 public interface ReqFriendRepositoryCustom {
 //    ReqFriends alreadyReq(Long myId, Long friendId); //이미 친구요청 보냈는지 확인
     ReqFriends checkSentReq(Long myId, Long friendId); //나한테 친구요청을 보낸 상태인지 확인
-    List<ReqFriends> listReq(Long myId); //받은 친구요청 목록
+    List<UserInfoDto> reqList(Long myId); //받은 친구요청 목록
     long delete(Long myId, Long friendId);
 }
