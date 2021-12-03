32ca5ee467e86c37f0381ac715ce30293fefbe71
seongmin <psm7335@naver.com>
2021-12-03 17:40:18 +0900

    add scheduleException

---
+++ src/main/java/glowa/glowabackendspring/exception/ScheduleException.java
@@ -0,0 +1,10 @@
+package glowa.glowabackendspring.exception;
+
+public class ScheduleException extends RuntimeException {
+    public ScheduleException() {
+    }
+
+    public ScheduleException(String message) {
+        super(message);
+    }
+}
