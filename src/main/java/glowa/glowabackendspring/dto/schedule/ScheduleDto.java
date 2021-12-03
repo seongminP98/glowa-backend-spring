cf83e7be54c236824ebd32ce298d8f3611a40c54
seongmin <psm7335@naver.com>
2021-12-03 17:40:33 +0900

    add scheduleDto

---
+++ src/main/java/glowa/glowabackendspring/dto/schedule/ScheduleDto.java
@@ -0,0 +1,23 @@
+package glowa.glowabackendspring.dto.schedule;
+
+import glowa.glowabackendspring.domain.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class ScheduleDto {
+    private User master;
+    private String name;
+    private String place;
+    private LocalDateTime date;
+
+    public ScheduleDto(User master, String name, String place, LocalDateTime date) {
+        this.master = master;
+        this.name = name;
+        this.place = place;
+        this.date = date;
+    }
+}
