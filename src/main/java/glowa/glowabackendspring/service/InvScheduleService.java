48929d2552088d641c0628fc95d4daadc4c6b694
seongmin <psm7335@naver.com>
2021-12-09 17:39:30 +0900

    add invScheduleService accept schedule

--- src/main/java/glowa/glowabackendspring/service/InvScheduleService.java
+++ src/main/java/glowa/glowabackendspring/service/InvScheduleService.java
@@ -95,4 +95,32 @@ public class InvScheduleService {
         return resultList;
 
     }
+
+    public Long accept(Long scheduleId, Long friendId, User me) {
+        Optional<Schedule> schedule = scheduleRepository.findOneById(scheduleId);
+        if (schedule.isEmpty()) {
+            throw new ScheduleException("잘못된 일정입니다. 다시 확인해주세요.");
+        }
+
+        Optional<User> friend = userRepository.findById(friendId);
+        if (friend.isEmpty()) {
+            throw new UserException("잘못된 유저입니다. 다시 확인해주세요.");
+        }
+
+        Optional<User> userMe = userRepository.findById(me.getId());
+        if (userMe.isEmpty()) {
+            throw new LoginException("잘못된 사용자입니다. 다시 로그인해주세요.");
+        }
+
+        Optional<InvSchedule> invited = invScheduleRepository.findOneByScheduleAndMeAndFriend(schedule.get(), userMe.get(), friend.get());
+        if(invited.isEmpty()) {
+            throw new ScheduleException("잘못된 스케줄입니다. 다시 확인해주세요.");
+        }
+
+        ScheduleManage saveSchedule = scheduleManageRepository.save(new ScheduleManage(userMe.get(), schedule.get()));
+
+        invScheduleRepository.deleteByScheduleAndMeAndFriend(schedule.get(),userMe.get(),friend.get());
+        return saveSchedule.getId();
+
+    }
 }
