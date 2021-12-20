07f6bfba76ed9b61fd0dd0878afe4c7810ae3de1
seongmin <psm7335@naver.com>
2021-12-20 22:28:40 +0900

    add FriendController exception handler

--- src/main/java/glowa/glowabackendspring/apiController/FriendApiController.java
+++ src/main/java/glowa/glowabackendspring/apiController/FriendApiController.java
@@ -2,7 +2,10 @@ package glowa.glowabackendspring.apiController;
 
 import glowa.glowabackendspring.domain.ReqFriends;
 import glowa.glowabackendspring.domain.User;
+import glowa.glowabackendspring.exception.FriendException;
 import glowa.glowabackendspring.exception.LoginException;
+import glowa.glowabackendspring.exception.UserException;
+import glowa.glowabackendspring.exhandler.ErrorResult;
 import glowa.glowabackendspring.payload.ResponseCode;
 import glowa.glowabackendspring.service.FriendService;
 import glowa.glowabackendspring.service.ReqFriendService;
@@ -10,6 +13,7 @@ import glowa.glowabackendspring.session.SessionConst;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +27,27 @@ public class FriendApiController {
 
     private final FriendService friendService;
     private final ReqFriendService reqFriendService;
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResult loginExHandler(LoginException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult(ResponseCode.CLIENT_ERROR, e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResult friendExHandler(FriendException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult(ResponseCode.CLIENT_ERROR, e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResult userExHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult(ResponseCode.CLIENT_ERROR, e.getMessage());
+    }
 
     @PostMapping("/add")
     public int addFriend(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) User user, @RequestBody @Validated AddFriendRequest request) {
@@ -38,6 +63,7 @@ public class FriendApiController {
         @NotEmpty(message = "친구 요청하는 아이디 값은 비어있을 수 없습니다.")
         private Long friendId;
     }
+    
 
 
 
