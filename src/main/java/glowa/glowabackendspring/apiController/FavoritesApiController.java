f87316f207065692f0f5fe37c39923aa25d012af
seongmin <psm7335@naver.com>
2021-12-21 15:45:15 +0900

    add Favorites Controller delete

--- src/main/java/glowa/glowabackendspring/apiController/FavoritesApiController.java
+++ src/main/java/glowa/glowabackendspring/apiController/FavoritesApiController.java
@@ -13,6 +13,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -44,17 +45,32 @@ public class FavoritesApiController {
 
     @PostMapping("/add")
     public int add(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) User user, @RequestBody @Validated FavoritesRequest request) {
+        if (user == null) {
+            throw new LoginException("로그인 되어있지 않음");
+        }
         favoritesService.add(new FavoritesInfoDto(request.restaurant, request.address, request.kind), user);
         return ResponseCode.OK;
     }
 
     @GetMapping("/list")
     public ListResponse list(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) User user) {
+        if (user == null) {
+            throw new LoginException("로그인 되어있지 않음");
+        }
         return new ListResponse(ResponseCode.OK, favoritesService.favoritesList(user));
     }
 
+    @DeleteMapping
+    public int delete(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) User user, @RequestParam("favoritesId") @Validated DeleteRequest request) {
+        if (user == null) {
+            throw new LoginException("로그인 되어있지 않음");
+        }
+        favoritesService.delete(user, request.favoritesId);
+        return ResponseCode.OK;
+    }
+
     @Data
-    static class FavoritesRequest{
+    static class FavoritesRequest {
         @NotEmpty(message = "식당 이름은 비어있을 수 없습니다.")
         private String restaurant;
         private String address;
@@ -63,8 +79,14 @@ public class FavoritesApiController {
 
     @Data
     @AllArgsConstructor
-    static class ListResponse{
+    static class ListResponse {
         private int conde;
         private List<Favorites> resultList;
+    }
+
+    @Data
+    static class DeleteRequest {
+        @NotEmpty(message = "id값은 비어있을 수 없습니다.")
+        private Long favoritesId;
     }
 }
